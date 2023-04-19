f3a80f1fcc1677f20f5e7508443afa372c627eca
Mary <itsmaryyang@gmail.com>
2023-04-18 21:55:21 -0500

    Add reactionSchema to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema({
     thoughtText: {
@@ -15,7 +16,7 @@ const thoughtSchema = new Schema({
         type: String,
         required: true,
     },
-    // reactions: [reactionSchema],
+    reactions: [reactionSchema],
 },
 {
     timestamps,
@@ -27,6 +28,6 @@ thoughtSchema.virtual('reactionCount').get(function(){
     return this.reactions.length;
 })
 
-const Thought = model('Thought', thoughtSchema)
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
