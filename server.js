d1220fdb593f9a6c85677222c6478c49f459441e
Mary <itsmaryyang@gmail.com>
2023-04-19 21:54:37 -0500

    modify imports and code in server.js

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +12,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
