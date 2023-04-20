9aa53d9d003ad772314e0efddd256662f109626a
Mary <itsmaryyang@gmail.com>
2023-04-20 13:32:51 -0500

    Add more data for seeding

--- utils/data.js
+++ utils/data.js
@@ -1,14 +1,19 @@
 const users = [
     {username: 'Kitty02', email: 'kitty02many@gmail.com'},
+    {username: 'Keroppi', email: 'keroppiSanrio@gmail.com'},
+    {username: 'Badtz-Maru', email: 'Badtz-Maru@comcast.net'},
+    {username: 'Gudetama', email: 'Gudetama@aol.com'},
 ];
 
 const thoughts = [
     {thoughtText: 'That was really cool!', username: 'Keroppi'},
     {thoughtText: 'I have another thought!', username: 'Badtz-Maru'},
+    {thoughtText: 'That was really amazing!', username: 'Gudetama'},
 ];
 
 const reactions = [
     {reactionBody: 'This is my reaction (O_O)', username: 'Gudetama'},
+    {reactionBody: 'A heart reaction <3 from me!', username: 'Kitty02'},
 ];
 
 module.exports = {users, thoughts, reactions};
--- utils/seed.js
+++ utils/seed.js
@@ -1,17 +1,25 @@
 const connection = require('../config/connection');
 const User = require('../models/User');
+const Thought = require('../models/Thought');
+const Reaction = require('../models/Reaction');
 const {users, thoughts, reactions} = require('./data');
 
+console.time('seeding');
 
 // Creates a connection to mongodb
 connection.once('open', async () => {
   // Delete the entries in the collection
   await User.deleteMany({});
+  await Thought.deleteMany({});
 
-  // Wait for the users to be inserted into the database
+
+  // Wait for the users and Thoughts to be inserted into the database
   await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
 
   console.table(users);
+  console.table(thoughts);
+  console.timeEnd('Seeding data complete!');
   process.exit(0);
 });
 
